56f9c90d17aecc52e896f9158ff9be16b34e39cd
Omar Mohamed <113803834+DarkShadow232@users.noreply.github.com>
2025-11-24 12:41:51 +0200

    Update Palindromic Linked List.cpp
    Removed comments explaining the problem and solutions, and cleaned up unnecessary comments in the code.

--- Palindromic Linked List.cpp
+++ Palindromic Linked List.cpp
@@ -1,19 +1,7 @@
-// PROBLEM :- https://leetcode.com/problems/palindrome-linked-list/
-
-// LEETCODE | EASY | LINKED LIST
-
-// Solution 1 :- T.C. O(N) S.C. O(N)
-// Take all the node values into a vector and check if the resultant vector is palindrome or not.
-
-// Solution 2 :- T.C. O(N) S.C. O(1)
-// Approach :- 
-// 1. Find Middle of the Linked List.
-// 2. Reverse the latter half of the Linked List.
-// 3. Compare corresponding nodes in the two halves of the lists.
+are corresponding nodes in the two halves of the lists.
 
 class Solution {
 public:
-    // reverse a linked list code
     ListNode* reverseList(ListNode* head) {
         
         ListNode* prev=NULL;
@@ -32,10 +20,8 @@ public:
         return prev;
     }
     
-    // actual function which gets called to check if a list is palindrome or not
     bool isPalindrome(ListNode* head) {
         
-        // find middle of the linked list
         ListNode* fast = head;
         ListNode* slow = head;
         while(fast != NULL && fast->next != NULL){
@@ -43,32 +29,20 @@ public:
             slow = slow->next;
         }
         
-        // reverse the latter half of the list
         ListNode* revHead = reverseList(slow);
         
-        // traverse in each half to check is it palindrome or not
         ListNode* firstHalf = head;
         ListNode* secondHalf = revHead;
         
         while(secondHalf != NULL){
             if(firstHalf->val != secondHalf->val){
-                return false; // not palindrome;
+                return false; 
             }
             firstHalf = firstHalf->next;
             secondHalf = secondHalf->next;
         }
         
-        return true; // palindrome
+        return true; 
     }
 };
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
