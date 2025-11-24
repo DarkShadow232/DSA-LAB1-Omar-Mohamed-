dec9b64cb4d97f94db14a24d264d1ebd44c8ae8e
Omar Mohamed <113803834+DarkShadow232@users.noreply.github.com>
2025-11-24 12:41:00 +0200

    Update Linked List Cycle.cpp
    Removed comments explaining the problem and solutions.

--- Linked List Cycle.cpp
+++ Linked List Cycle.cpp
@@ -1,19 +1,3 @@
-// PROBLEM :- https://leetcode.com/problems/linked-list-cycle/
-
-// LEETCODE | EASY | LINKED LIST
-
-// Solution 1 :- T.C :- O(N) S.C :- O(N)
-
-// Use a SET to store the NODES, and insert the nodes while traversing the list. If there is a cycle, you will find some node X already in the list previously. This node X is the Cyclic Intersection node. 
-// Return TRUE if you detect a cycle. Else if you reach the end of list, return FALSE.
-
-// Solution 2 :- T.C. :- O(N) S.C :- O(1)
-
-// Classic Tortoise Hare Problem. 
-// Initialize both of the pointers at HEAD node. 
-// And move SLOW pointer 1 step at a time while FAST pointer 2 step at a time.
-// If there is a cycle, both pointers are bound to meet at a point.
-// If FAST pointer reaches the end of the list, then there is NO CYCLE present.
 
 class Solution {
 public:
@@ -34,12 +18,3 @@ public:
         return false;
     }
 };
-
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
