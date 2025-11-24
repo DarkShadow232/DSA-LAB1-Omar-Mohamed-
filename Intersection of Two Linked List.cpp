5258770d017acdbd2b707154d976567dc03d232c
Omar Mohamed <113803834+DarkShadow232@users.noreply.github.com>
2025-11-24 12:40:15 +0200

    Clean up comments in Intersection of Two Linked List.cpp
    Removed commented-out problem statement and solutions from the Intersection of Two Linked List implementation.

--- Intersection of Two Linked List.cpp
+++ Intersection of Two Linked List.cpp
@@ -1,14 +1,3 @@
-// PROBLEM :- https://leetcode.com/problems/intersection-of-two-linked-lists/
-
-// LEETCODE | EASY | LINKED-LIST
-
-// Solution 1 :- T.C. O(N + M) S.C. O(1)
-
-// Count the number of nodes of two lists. 
-// Skip the additional greater nodes from the larger list, so that equal number of nodes remain in both lists.
-// Then traverse both lists together.
-// If there is a match, return that NODE.
-// In the end of list, if there is no match, return NULL.
 
 
 class Solution {
@@ -56,14 +45,6 @@ public:
     }
 };
 
-// Solution 2 :- T.C. O(N + M) S.C. O(1) (ALTERNATIVE APPROCH)
-
-// Take two pointers D1 and D2 initially at head of ListA and ListB respectively.
-// Start traversing in both linked list. Increement both pointers by 1 step at each step.
-// Whenever D1 reaches end of ListA, make it point to ListB head.
-// Similarly when D2 reaches at the end of ListB, make it point to ListA head.
-
-// After at max 2 times whole traversal of both lists, both pointers will reach at an common intersection point. Or they simultaneously will reach at NULL, referring no intersection.
 
 class Solution {
 public:
@@ -91,11 +72,5 @@ public:
     }
 };
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
+
  */
