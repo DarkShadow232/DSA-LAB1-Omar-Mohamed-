6a5fcbc2d58a1086d92a268629b02088663af34a
Omar Mohamed <113803834+DarkShadow232@users.noreply.github.com>
2025-11-24 12:42:29 +0200

    Update Rotate List By K Times.cpp
    Removed comments explaining the problem and solution approach.

--- Rotate List By K Times.cpp
+++ Rotate List By K Times.cpp
@@ -1,19 +1,3 @@
-// PROBLEM :- https://leetcode.com/problems/rotate-list/
-
-// LEETCODE | MEDIUM | LINKED LIST
-
-// Solution :- T.C. O(N) S.C O(1)
-
-// First count (CNT) the number of nodes.
-// Now we know every CNT times, the list retains to the initial position. So K = K % CNT.
-// If K = 0, awesome, no rotation, return head as it is.
-
-// Now lets suppose K = 2 in the list 1->2->3->4->5->NULL.
-// Last 2 nodes comes in front of list.
-// So device a strategy to split the list at K nodes from back (1->2->3->NULL and 4->5->NULL)
-// Now attach 5 (last node) next to 1 (initial head).
-// And return 4 (new head).
-// Where to split ? After CNT - K - 1 iterations from start HEAD.
 
 class Solution {
 public:
@@ -53,13 +37,3 @@ public:
     }
 };
 
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
